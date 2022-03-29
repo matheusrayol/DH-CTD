3c6e17df1d7dd191af810162497a642a1cc43eb9
Matheus Rayol <matheusrbferreira@gmail.com>
2022-03-28 21:19:22 -0300

    Ajuste de numeração das aulas; Inclusão do conteúdo das aulas de 28 de março;

--- Infraestrutura-I/Aula-08/aula08.py
+++ Infraestrutura-I/Aula-08/aula08.py
@@ -1,2 +1,37 @@
 nome = "Matheus"
 print(nome)
+nome = 36
+print(nome)
+verdade = True
+print(verdade)
+altura = 1.84
+print(altura)
+teste = verdade
+print(teste)
+
+lista_alunos = ["Matheus", "João", "Maria"]
+lista_alunos.append("Pedro")
+
+print(lista_alunos)
+print(len(lista_alunos))
+
+lista_alunos.remove("João")
+print(lista_alunos)
+
+
+dados_pessoais = {"nome": "Matheus", "idade": "18", "altura": "1.84"}
+
+print(dados_pessoais["nome"])
+
+
+
+idade = 9
+
+if idade < 18:
+    print("Menor de idade")
+elif idade >= 18 and idade < 30:
+    print("Adulto")
+elif idade >= 30 and idade < 60:
+    print("Adulto")
+else:
+    print("Idoso") 
